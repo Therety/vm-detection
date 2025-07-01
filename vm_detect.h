2ef4628777a41f3406150cab7933fb3d54bd7bea
Michal Rety <42116839+Therety@users.noreply.github.com>
2025-07-01 21:10:59 +0200

    detections

--- vm_detect.h
+++ vm_detect.h
@@ -7,11 +7,17 @@ const char* detect_os();
 // Platform-independent VM check
 int is_vm();
 
-// Platform-specific checks
+// Detection checks
 int is_vm_cpuid();
 int is_vm_vendor();
 int is_vm_rdtsc();
 int is_vm_vmware_io();
 int is_vm_mac();
+int is_vm_sidt();
+int is_vm_sgdt();
+int is_vm_smsw();
+int is_vm_cpuid_jitter();
+int is_vm_rdtsc_vs_qpc();
+int is_vm_throttling();
 
 #endif
