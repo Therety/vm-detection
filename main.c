3265e4847da4fed8257404add7573080524c0193
Michal Rety <42116839+Therety@users.noreply.github.com>
2025-07-01 21:10:30 +0200

    detections

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main() {
     const char* os = detect_os();
     printf("Detected OS: %s\n", os);
 
+    // this is mainly for debugging, but i'm leaving it in :D
     if (is_vm_cpuid()) {
         printf("[!] Hypervisor bit is SET\n");
     }
@@ -20,6 +21,25 @@ int main() {
     if (is_vm_mac()) {
         printf("[!] Virtual MAC address prefix detected\n");
     }
+    if (is_vm_sidt()) {
+        printf("[!] IDT in lower memory range\n");
+    }
+    if (is_vm_sgdt()) {
+        printf("[!] GDT in lower memory range\n");
+    }
+    if (is_vm_smsw()) {
+        printf("[!] MSW odd value\n");
+    }
+    if (is_vm_cpuid_jitter()) {
+        printf("[!] Lower CPU jitter\n");
+    }
+    if (is_vm_rdtsc_vs_qpc()) {
+        printf("[!] Odd rdtsc value\n");
+    }
+    if (is_vm_throttling()) {
+        printf("[!] Slow rdtsc progression\n");
+    }
+
 
     if (is_vm()) {
         printf("Running in a virtual environment!\n");
