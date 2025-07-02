3f607ad871f4ad0b8758c94a63f041438c3a2dc0
Michal Rety <42116839+Therety@users.noreply.github.com>
2025-07-02 09:55:34 +0200

    removing some functions; adding is_vm_clflush_timing()

--- vm_detect.h
+++ vm_detect.h
@@ -9,15 +9,13 @@ int is_vm();
 
 // Detection checks
 int is_vm_cpuid();
-int is_vm_vendor();
 int is_vm_rdtsc();
-int is_vm_vmware_io();
-int is_vm_mac();
 int is_vm_sidt();
 int is_vm_sgdt();
 int is_vm_smsw();
 int is_vm_cpuid_jitter();
 int is_vm_rdtsc_vs_qpc();
 int is_vm_throttling();
+int is_vm_clflush_timing();
 
 #endif
